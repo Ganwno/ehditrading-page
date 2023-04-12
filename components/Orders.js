1c8e5d85633c0a9d6037aa73a0618e6a8223d5cc
Mohammad Poormehdi <mohammad.poormehdi1383@gmail.com>
2023-04-12 19:32:58 +0430

    chart size on mobile fixed

--- components/Orders.js
+++ components/Orders.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function Orders(){
     return(
-        <div className="w-full bg-slate-700 h-[30%] py-5 px-10 max-md:hidden ">
+        <div className="w-full bg-slate-700 h-[30%] py-5 px-10 max-md:hidden">
             <div className="flex gap-7 text-white">
                 <div className="py-2">Orders</div>
                 <div className="py-2">Pending</div>
